fefe31e00b1aa9674888ce2f2d6d8778eb66e1f0
RoerMadsen <145968234+RoerMadsen@users.noreply.github.com>
2024-02-19 12:51:53 +0100

    projekt oprettet

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/*importerer soruce, destination, watch og serie funktioner fra Gulp */
+const {src, dest, watch, series } = require('gulp')
+/*Importerer Gulp-sass plugin fra Gulp */
+const sass = require ('gulp-sass')(require('sass'))
+
+/** Opret funktion der compiler sass til css
+ * Tager Sass fra sourcefilen - index.scss og kører det gennem sass plugin, der omdanner det til en css mappe
+*/
+function buildStyles() {
+    return src('index.scss')
+        .pipe(sass())
+        .pipe(dest('css'))
+}
+
+/*Laver en watch funktion, der automatisk overfører rettelser fra sass til css */
+function watchTask() {
+    watch(['index.scss'], buildStyles)
+}
+
+/** Bruger series funktion til at eksportere filerne i korrekt rækkefølge */
+exports.default = series(buildStyles, watchTask)
+
+/**
+ * Åben Terminal og tast npm install gulp gulp-sass sass --save-dev
+ *  derefter "gulp" for at compile */
+
